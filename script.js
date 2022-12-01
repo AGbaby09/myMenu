3e66fc99f9159b26ebb57ba699fe342c1af82a1c
AGbaby09 <116189737+AGbaby09@users.noreply.github.com>
2022-12-01 12:56:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+        id: 10,
+        title: "juicy steak",
+        category: "steak",
+        price: 36.99,
+        img: "./images/item-10.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+];
+//rendering data in your html
+const section = document.querySelector('section');
+const buttonDiv = document.querySelectorAll("header ul")[0]
+console.log(buttonDiv)
+  
+//load items
+window.addEventListener('DOMContentLoaded', ()=>{
+    displayItems(menu);
+    
+    //set categories
+    displayButtons()
+});
+
+//display buttons
+function displayButtons(
+    ){
+        const categories = menu.reduce((values, item)=>{
+            if(!values.includes(item.category)){
+                values.push(item.category)
+            }
+            return values;
+        }, ['all']);
+        let categoryButtons = categories.map((butt)=>{
+            
+            return `<li><button type="button" data-id=${butt}>${butt}</button></li>`;
+        }).join("");
+        buttonDiv.innerHTML = categoryButtons
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach((btn)=>{
+            btn.addEventListener('click', (key)=>{
+                const category= key.currentTarget.dataset.id;
+                const menuCat = menu.filter((menuItem)=>{
+                    if(menuItem.category===category){
+                        return menuItem;
+                    }
+                });
+                if(category==="all"){
+                    displayItems(menu);
+                }else{
+                    displayItems(menuCat);
+                }
+            })
+        })
+    }
+    
+    
+    
+//filter
+
+function displayItems(menuItems){
+    let displayMenu = menuItems.map((item)=>{
+        
+        return `<article>
+        <img class="image" src=${item.img} alt="">
+        <div class="details">
+        <h4 class="title">${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+        </div>
+        <div class="info">
+            <p>${item.desc}</p>
+        </div>
+    </article>`;
+    })
+    displayMenu = displayMenu.join("");
+    section.innerHTML = displayMenu
+  }
